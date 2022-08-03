b374732dcfdbea56fc6f70e15c8febdae7f5737f
Crowdbotics <team@crowdbotics.com>
2022-08-03 21:05:19 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/jayrytthm_sound_of__36587/MainActivity.java
@@ -0,0 +1,15 @@
+package com.jayrytthm_sound_of__36587;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "jayrytthm_sound_of__36587";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "jayrytthm_sound_of__36587",
+  url: "https://jayrytthm_sound_of__36587.botics.co",
+  api: "https://jayrytthm_sound_of__36587.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
